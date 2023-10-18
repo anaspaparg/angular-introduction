fda363a458713b407101382ebd01341499918a97
An. Papargyropoulos <anaspaparg@gmail.com>
2023-10-18 10:29:23 +0200

    Users Crud Scaffolding

---
+++ src/app/crud-demo/crud-demo/crud-demo.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CreateUserComponent } from "../create-user/create-user.component";
+import { ReadUserComponent } from '../read-user/read-user.component';
+import { UpdateUserComponent } from '../update-user/update-user.component';
+import { DeleteUserComponent } from '../delete-user/delete-user.component';
+import { ListUsersComponent } from '../list-users/list-users.component';
+
+@Component({
+    selector: 'app-crud-demo',
+    standalone: true,
+    templateUrl: './crud-demo.component.html',
+    styleUrls: ['./crud-demo.component.css'],
+    imports: [CommonModule, CreateUserComponent, ReadUserComponent, UpdateUserComponent,
+              DeleteUserComponent, ListUsersComponent]
+})
+export class CrudDemoComponent {
+  crudOperation: string | undefined;
+
+  onSelection(operation: string) {
+    this.crudOperation = operation
+  }
+}
