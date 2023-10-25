de2db1f473649cf8c63d816e370323e948d252a5
An. Papargyropoulos <anaspaparg@gmail.com>
2023-10-25 14:38:26 +0200

    user search and are you sure component usage

---
+++ src/app/crud-demo/utils/crud-user-search/crud-user-search.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-crud-user-search',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './crud-user-search.component.html',
+  styleUrls: ['./crud-user-search.component.css']
+})
+export class CrudUserSearchComponent {
+
+}
